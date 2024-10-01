31e052720f737a6862271290e0a7b73224dbd77b
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-10-01 09:30:31 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function divideAndConquerSum(a) {
-    return a;
+    return getSums(a,0,a.length());
+}
+function getSums(a,low, high) {
+    const third = Math.floor((high-low)/3);
+    const secondthird = low + third;
+    const thirdthird = secondthird + third;
+
+    const sumfirstThird = getSums(a, low, third);
+    const sumsecondThird = getSums(a,third, secondthird);
+    const sumthirdThird = getSums(a,secondthird,thirdthird);
+
+    return (sumfirstThird + sumsecondThird + sumthirdThird);
 }
