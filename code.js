05a9ad33f4259651af635b763b33ebd9922b8736
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-10-01 10:22:30 -0600

    Update code.js

--- code.js
+++ code.js
@@ -14,11 +14,11 @@ function getSums(a,low, high) {
     //high - low makes sure we stay within the bounds, divide by three to make thirds.
     const third = Math.floor((high-low)/3);
     const firstbreak = low + third;
-    const secondbreak = firstbreak + third;
+    const secondbreak = firstbreak + third + 1;
 
     //recursively calls each third to get the thirds and sums of each of the others.
-    const sumfirstThird = getSums(a, low, firstbreak);
-    const sumsecondThird = getSums(a,firstbreak, secondbreak);
+    const sumfirstThird = getSums(a, low, firstbreak + 1);
+    const sumsecondThird = getSums(a,firstbreak + 1, secondbreak);
     const sumthirdThird = getSums(a,secondbreak,high);
 
     //returns the sum of each third.
