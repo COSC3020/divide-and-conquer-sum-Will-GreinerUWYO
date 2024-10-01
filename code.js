27729a2e7fc3f7fb32825e2b68af0a616d058f9c
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-10-01 09:40:05 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,13 +2,25 @@ function divideAndConquerSum(a) {
     return getSums(a,0,a.length());
 }
 function getSums(a,low, high) {
+    //gone through the whole array
+    if(high - low <= 0) {
+        return 0;
+    }
+    //just return, no more sums, reached array of length one.
+    if(high == low) {
+        return a[low];
+    }
+
+    //high - low makes sure we stay within the bounds, divide by three to make thirds.
     const third = Math.floor((high-low)/3);
     const secondthird = low + third;
     const thirdthird = secondthird + third;
 
+    //recursively calls each third to get the thirds and sums of each of the others.
     const sumfirstThird = getSums(a, low, third);
     const sumsecondThird = getSums(a,third, secondthird);
     const sumthirdThird = getSums(a,secondthird,thirdthird);
 
+    //returns the sum of each third.
     return (sumfirstThird + sumsecondThird + sumthirdThird);
 }
